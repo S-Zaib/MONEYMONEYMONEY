46f5b2ebe2b1240306cd1a1e981d482ed2b5558b
Administrator <Administrator@DESKTOP-32K92IM>
2022-12-01 15:33:01 +0500

    Add project files.

---
+++ MONEYMONEYMONEY/pieces.h
@@ -0,0 +1,4 @@
+/* PROGRAMMING FUNDAMENTAL'S PROJECT FOR FALL 2022 BS(CS)
+ * Shape of each piece is represented by rows in the array.
+ * TIP: Name the array what is already been coded to avoid any unwanted errors.
+ */
